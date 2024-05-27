d335c33481624038527cd5257aa24a87a6243253
James Baker <jamesjbaker@tutanota.com>
2024-05-27 03:57:48 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureToConvert) {
+  let convertedTemperature = (temperatureToConvert - 32) * (5/9);
+  convertedTemperature = Math.round(convertedTemperature * 10) / 10;
+
+  return convertedTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureToConvert) {
+  let convertedTemperature = temperatureToConvert * (9/5) + 32;
+  convertedTemperature = Math.round(convertedTemperature * 10) / 10;
+
+  return convertedTemperature;
 };
 
 // Do not edit below this line
