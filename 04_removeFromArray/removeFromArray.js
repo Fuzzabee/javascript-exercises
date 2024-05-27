9e503718a207a9f6c4d5aa4352db27c3a69e3e42
James Baker <jamesjbaker@tutanota.com>
2024-05-27 03:30:57 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, toRemove) {
+    toRemove = Array.from(arguments).slice(1);
+
+    newArray = [];
+    for (const item of originalArray) {
+        if (!toRemove.includes(item)) {
+            newArray.push(item);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
