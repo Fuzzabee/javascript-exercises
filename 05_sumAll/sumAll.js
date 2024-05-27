fa352cd02dc7fe949f8e9d8acae6c1844c7e6b19
James Baker <jamesjbaker@tutanota.com>
2024-05-27 03:38:11 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof firstNumber !== "number" ||
+        typeof secondNumber !== "number" ||
+        firstNumber < 0 ||
+        secondNumber < 0) {
+            return "ERROR";
+        }
 
+    let sum = 0;
+    let min = Math.min(firstNumber, secondNumber);
+    let max = Math.max(firstNumber, secondNumber);
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
