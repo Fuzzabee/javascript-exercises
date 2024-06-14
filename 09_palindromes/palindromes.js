62bc23c235a618cb43c4fe8c86d6b39eb50b4719
James Baker <jamesjbaker@tutanota.com>
2024-06-14 17:03:33 -0400

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+    let original = string
+                    .toLowerCase()
+                    .split("")
+                    .filter((char) => alphaNumeric.includes(char))
+                    .join("");
+
+    let reversed = original
+                    .split("")
+                    .reverse()
+                    .join("");
+
+    return original === reversed;
 };
 
 // Do not edit below this line
