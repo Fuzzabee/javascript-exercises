907708c534adb9e2561c44973b6fc03dc893bed9
James Baker <jamesjbaker@tutanota.com>
2024-06-14 04:03:43 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
 const add = function() {
-	
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	return arguments[0].reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
 const multiply = function() {
-
+  return arguments[0].reduce((total, current) => {
+    return total * current;
+  }, 1);
 };
 
 const power = function() {
-	
+  return Math.pow(arguments[0], arguments[1]);
 };
 
 const factorial = function() {
-	
+	if (arguments[0] === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = arguments[0]; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
