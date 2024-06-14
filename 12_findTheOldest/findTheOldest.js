687ba793a24355c69313ed4d5caa71109a74e8e5
James Baker <jamesjbaker@tutanota.com>
2024-06-14 17:30:57 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    let sortedOldestFirst = people.sort((first, second) => {
+        let firstBirth = first.yearOfBirth;
+        let firstDeath = !first.yearOfDeath ? currentYear : first.yearOfDeath;
+        let firstAge = firstDeath - firstBirth;
 
+        let secondBirth = second.yearOfBirth;
+        let secondDeath = !second.yearOfDeath ? currentYear : second.yearOfDeath;
+        let secondAge = secondDeath - secondBirth;
+
+        return secondAge - firstAge;
+    });
+
+    return sortedOldestFirst[0];
 };
 
 // Do not edit below this line
