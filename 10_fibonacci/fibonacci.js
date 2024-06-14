b74c34bc5156e56683d59efeb02458675216212b
James Baker <jamesjbaker@tutanota.com>
2024-06-14 17:09:26 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num < 0) {
+        return "OOPS";
+    }
 
+    if (+num === 0 ) {
+        return 0;
+    }
+
+    let prev = 0;
+    let current = 1;
+    for (let i = 1; i < num; i++) {
+        let temp = prev;
+        prev = current;
+        current += temp;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
